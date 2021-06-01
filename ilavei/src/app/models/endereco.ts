4670dc3b7bea5d353bd4e9f199c8e44d0cacb76d
Devsenac2019 <devsenac2019.1@gmail.com>
2021-05-31 21:24:44 -0300

    Lucas

---
+++ ilavei/src/app/models/endereco.ts
@@ -0,0 +1,13 @@
+export class Endereco {
+
+    numero: string;
+    complemento: string;
+    
+    cep: string;
+    logradouro: string;
+    bairro: string;
+    localidade: string;
+    uf: string;
+
+    erro:boolean = false;
+}
