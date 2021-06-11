b3ab00286bfdde6497df56019d06e12d8b39423a
Devsenac2019 <devsenac2019.1@gmail.com>
2021-06-11 20:25:37 -0300

    lc

---
+++ ilavei/src/app/models/servico.ts
@@ -0,0 +1,12 @@
+export class Servico {
+    nome:string;
+    avaliservico: string;
+    cancelamento: string;
+    dataservico: string;
+    descricao: string;
+    ordeservico: string;
+    pagamento: string = null;
+    reclamacao: string;
+    erro: boolean = false;
+
+}
