38f562ac63584bcc4b344d25f1848cb361d2b15d
Devsenac2019 <devsenac2019.1@gmail.com>
2021-06-11 18:43:50 -0300

    LC

--- ilavei/src/app/tab4/tab4-routing.module.ts
+++ ilavei/src/app/tab4/tab4-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule,Routes } from '@angular/router';
-import { Tab3Page } from './tab3.page';
+import { Tab4Page } from './tab4.page';
 
 const routes: Routes = [
   {
     path: '',
-    component: Tab3Page,
+    component: Tab4Page
   }
 ];
 
@@ -13,4 +13,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class Tab3PageRoutingModule {}
+export class Tab4PageRoutingModule {}
