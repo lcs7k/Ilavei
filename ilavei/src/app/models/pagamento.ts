37716a3bb96a81aeffe51483df67bead573de910
Devsenac2019 <devsenac2019.1@gmail.com>
2021-06-30 19:51:05 -0300

    banco pagamento

--- ilavei/src/app/models/pagamento.ts
+++ ilavei/src/app/models/pagamento.ts
@@ -1,8 +1,14 @@
-  
+import { Ordem } from "./ordem";
+
+
 export class Pagamento {
-    pix:string;
+
+    userkey: string;
+    listaordem: Ordem[] = [];
+    pix: string ;
     dinheiro: string;
     debito: string;
     credito: string;
-    
-}   
+    data: Date = new Date;
+
+}
